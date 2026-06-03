8dfaa360751e7ccdb879f2fc153e4fe81e7a42d4
KaiFon-2 <brawldeniska@gmail.com>
2026-06-03 14:48:59 +0300

    Update models.py

--- S15/models.py
+++ S15/models.py
@@ -1,35 +1,27 @@
-from peewee import *
+from peewee import Model, SqliteDatabase, AutoField, IntegerField, BooleanField
+from contextlib import closing
 
 db = SqliteDatabase('load_assignment.db')
 
-class BaseModel(Model):
+
+class LoadAssignment(Model):
+    id = AutoField(primary_key=True, column_name='id')
+    teacher_id = IntegerField(column_name='teacher_id', null=False)
+    discipline_id = IntegerField(column_name='discipline_id', null=False)
+    group_id = IntegerField(column_name='group_id', null=False)
+    hours_total = IntegerField(column_name='hours_total', null=False)
+    is_active = BooleanField(column_name='is_active', default=True, null=False)
+
     class Meta:
         database = db
-
-class Teacher(BaseModel):
-    id = AutoField(primary_key=True)
-    full_name = CharField(max_length=100)  # ФИО преподавателя
-    email = CharField(unique=True, max_length=100)  # Email
-    is_active = BooleanField(default=True)  # Логическое удаление
+        table_name = 'LOAD_ASSIGNMENT'
 
 
-class Discipline(BaseModel):
-    id = AutoField(primary_key=True)
-    name = CharField(max_length=100)  # Название дисциплины (Математика, Физика и т.д.)
-    hours_per_semester = IntegerField()  # Количество часов в семестре
-    is_active = BooleanField(default=True)
+def init_db():
+    with closing(db):
+        db.connect()
+        db.create_tables([LoadAssignment], safe=True)
 
-class Group(BaseModel):
-    id = AutoField(primary_key=True)
-    name = CharField(unique=True, max_length=50)  # Название группы (101-ИСТ)
-    year_formed = IntegerField()  # Год формирования
-    is_active = BooleanField(default=True)
 
-class LoadAssignment(BaseModel):  # Основная таблица распределения нагрузки
-    id = AutoField(primary_key=True)
-    teacher = ForeignKeyField(Teacher, backref='assignments')
-    discipline = ForeignKeyField(Discipline, backref='assignments')
-    group = ForeignKeyField(Group, backref='assignments')
-    semester = CharField(max_length=20)  # Семестр (Осенний 2024, Весенний 2025)
-    hours_assigned = IntegerField()  # Назначенные часы
-    is_active = BooleanField(default=True)  # Логическое удаление
+if __name__ == '__main__':
+    init_db()
